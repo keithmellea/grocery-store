4adfaa93d96a804c28b16ee8fce23d9343996123
Keith Mellea <keithmellea@gmail.com>
2021-04-20 18:46:02 -0400

    addcart done

--- src/store/cart.js
+++ src/store/cart.js
@@ -5,7 +5,7 @@ const ADD = "cart/ADD"
 export const addToCart = (newItem) => {
    return {
       type: ADD,
-      newItem.id: {
+      newItem: {
          id: newItem.id,
          count: 1,
       },
@@ -14,6 +14,14 @@ export const addToCart = (newItem) => {
 
 export const cartReducer = (state = {}, action) => {
    switch (action.type) {
+       case ADD:
+           const newState = {...state };
+           if (newState[action.newItem.id] === undefined) {
+           newState[action.newItem.id] = action.newItem;
+           } else {
+               newState[action.newItem.id].count++;
+           }
+           return newState;
       default:
          return state;
    }
